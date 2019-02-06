8f9676e0bd24b6dfa3111f89e4b772145cda2786
Marcel Ribeiro Dantas <mribeirodantas@protonmail.com>
2019-02-06 02:58:08 +0100

    Corrected code according to RStudio Check
    Changed folders, files and scripts according to notes/warning/errors from
    RStudio Check (Build)

---
+++ R/preprocessSEER.R
@@ -0,0 +1,28 @@
+#' Reads a sas instruction file in order to parse SEER ASCII data files
+#' @param file_path A path to the SAS file
+#' @return A data frame with instruction for readSEER
+#' @examples
+#' # You must preprocess a SAS file before reading the data with readSEER
+#' # Do not run
+#' # preprocessSEER("read.seer.research.nov17.sas")
+#' @import readr dplyr stringr 
+#' @export
+preprocessSEER <- function(file_path) {
+  sas.raw <- readr::read_lines(file_path)
+  sas.df <<- dplyr::tibble(raw = sas.raw) %>% 
+    ## remove first few rows by insisting an @ that defines the start index of that field
+    dplyr::filter(stringr::str_detect(raw, "@")) %>% 
+    ## extract out the start, width and column name+description fields
+    dplyr::mutate(start = stringr::str_replace(stringr::str_extract(raw, "@ [[:digit:]]{1,3}"), "@ ", ""),
+           width = stringr::str_replace(stringr::str_extract(raw, "\\$char[[:digit:]]{1,2}"), "\\$char", ""),
+           col_name = stringr::str_extract(raw, "[[:upper:]]+[[:upper:][:digit:][:punct:]]+"),
+           col_desc = stringr::str_trim(stringr::str_replace(stringr::str_replace(stringr::str_extract(raw, "\\/\\*.+\\*\\/"), "\\/\\*", ""), "\\*\\/", "" )) ) %>% 
+    ## coerce to integers
+    dplyr::mutate_at(dplyr::vars(start, width), dplyr::funs(as.integer)) %>% 
+    ## calculate the end position
+    dplyr::mutate(end = start + width - 1)
+  
+  column_mapping <- sas.df %>% 
+    dplyr::select(col_name, col_desc)
+}
+
--- R/readSEER.R
+++ R/readSEER.R
@@ -1,92 +1,122 @@
-# function to read SEER data from one or several ASCII data files
-readSEER <- function(path, year_dx, state, cancer, site, fips) {
-  if (missing(path)) {
+#' Reads SEER data from one or several SEER ASCII data files
+#' @param paths A path or paths to SEER ASCII data files
+#' @param year_dx Filter the result to this year of diagnosis (or range of years of diagnosis)
+#' @param primary_site Filter the result to this primary site
+#' @return A data frame with all SEER data from the ASCII data files you provided, given the criteria you chose in the parameters.
+#' @examples
+#' # You must preprocess a SAS file before, so that readSEER
+#' # knows the format of your SEER ASCII data files
+#' # Do not run
+#' # preprocessSEER("read.seer.research.nov17.sas")
+#' 
+#' # Now you can read it
+#' # Do not run
+#' # paths = c("/home/user/SEER/yr1973_2015.seer9/BREAST.TXT",
+#' #           "/home/user/SEER/yr2000_2015.ca_ky_lo_nj_ga/BREAST.TXT")
+#'
+#' # I'm interested here in patients with Breast cancer diagnosed
+#' # between 2012 and 2015
+#' # Do not run
+#' # seer_data <- readSEER(paths, c(2012:2015), site="Breast")
+# If you don't use export, users can't see it
+#' @import readr crayon 
+#' @export
+readSEER <- function(paths, year_dx, primary_site) {
+  if (missing(paths)) {
     stop("It's impossible to read the files if you supply no path to files.")
   }
   ## read the file with the fixed width positions
   data.df_f <- NULL
   for (path_file in paths) {
-    data.df <- read_fwf(path_file, 
-                        fwf_positions(sas.df$start, sas.df$end, sas.df$col_name)
+    data.df <- readr::read_fwf(path_file, 
+                               readr::fwf_positions(sas.df$start, sas.df$end, sas.df$col_name)
                         # , colClasses=c("character","integer","character","integer","character","integer","character",
                         #              "character","character","character"))
     )
     data.df_f <- rbind(data.df_f, data.df)
   } 
   # select only those rows that match the chosen site
-  if (missing(site)) {
-    print(paste(get(red("Note:"))," Cancer primary site not supplied, including all sites"))
+  if (missing(primary_site)) {
+    print(paste(cat(crayon::red("Note:"))," Cancer primary site not supplied, including all sites"))
   } else {
-    data.df_f <- data.df_f[!(data.df_f$SITERWHO == siteLookUp(site)), ] 
+    data.df_f <- data.df_f[data.df_f$SITERWHO == siteLookUp(primary_site), ] 
   }
   # select only those rows that match the chosen year of diagnosis (or range of)
-  if (missing(site)) {
-    print(paste(get(red("Note:"))," Year of diagnosis not supplied, including all years"))
+  if (missing(year_dx)) {
+    print(paste(cat(crayon::red("Note:"))," Year of diagnosis not supplied, including all years"))
   } else {
     data.df_f <- data.df_f[data.df_f$YEAR_DX %in% year_dx, ] 
   }
+  return (data.df_f)
 }
 
-# Convert primary site name to recode
+# Gotta hide it from the help page
+#' Converts primary site name to recode
+#' @param site_name cancer primary site name
+#' @return Recode code for the specified cancer primary site name
+#' @import stringr utils
 siteLookUp <- function(site_name) {
+  site_name <- stringr::str_to_title(site_name)
   if(!exists("sites_recodes")) {
-    sites_recodes <- readRDS("/home/mribeirodantas/Documentos/PhD data/Breast Cancer Analysis/vidente/sites_recodes.RData")
+    utils::data("sites_recodes")
   }
   return (subset(sites_recodes, sites == site_name)$recodes)
 }
 
-# List cancers primary sites that are supported by this package
+#' List cancers primary sites that are supported by this package
+#' @return It displays on the screen the cancer primary sites supported by the package
+#' @export
 listPrimarySites <- function() {
   if(!exists("sites_recodes")) {
     sites_recodes <- readRDS("/home/mribeirodantas/Documentos/PhD data/Breast Cancer Analysis/vidente/sites_recodes.RData")
   }
-  cat(red("# Oral Cavity and Pharynx"))
+  cat(crayon::red("# Oral Cavity and Pharynx"))
   print(paste(sites_recodes$sites[1:10], collapse = ", "))
-  cat(red("# Digestive System"))
+  cat(crayon::red("# Digestive System"))
   print(paste(sites_recodes$sites[11:13], collapse = ", "))
-  cat(red("# Colon Excluding rectum"))
+  cat(crayon::red("# Colon Excluding rectum"))
   print(paste(sites_recodes$sites[14:22], collapse = ", "))
-  cat(red("# Rectum and Rectosigmoid Junction"))
+  cat(crayon::red("# Rectum and Rectosigmoid Junction"))
   print(paste(sites_recodes$sites[23:25], collapse = ", "))
-  cat(red("# Liver and Intrahepatic Bile Duct"))
+  cat(crayon::red("# Liver and Intrahepatic Bile Duct"))
   print(paste(sites_recodes$sites[26:33], collapse = ", "))
-  cat(red("# Respiratory System"))
+  cat(crayon::red("# Respiratory System"))
   print(paste(sites_recodes$sites[34:38], collapse = ", "))
-  cat(red("# Bones and Joints"))
+  cat(crayon::red("# Bones and Joints"))
   print(paste(sites_recodes$sites[39], collapse = ", "))
-  cat(red("# Soft Tissue including Heart"))
+  cat(crayon::red("# Soft Tissue including Heart"))
   print(paste(sites_recodes$sites[40], collapse = ", "))
-  cat(red("# Breast"))
+  cat(crayon::red("# Breast"))
   print(paste(sites_recodes$sites[41], collapse = ", "))
-  cat(red("# Female Genital System"))
+  cat(crayon::red("# Female Genital System"))
   print(paste(sites_recodes$sites[42:48], collapse = ", "))
-  cat(red("# Male Genital System"))
+  cat(crayon::red("# Male Genital System"))
   print(paste(sites_recodes$sites[49:52], collapse = ", "))
-  cat(red("# Urinary System"))
+  cat(crayon::red("# Urinary System"))
   print(paste(sites_recodes$sites[53:56], collapse = ", "))
-  cat(red("# Eye and Orbit"))
+  cat(crayon::red("# Eye and Orbit"))
   print(paste(sites_recodes$sites[57], collapse = ", "))
-  cat(red("# Brain and Other Nervous System"))
+  cat(crayon::red("# Brain and Other Nervous System"))
   print(paste(sites_recodes$sites[58:60], collapse = ", "))
-  cat(red("# Endocrine System"))
+  cat(crayon::red("# Endocrine System"))
   print(paste(sites_recodes$sites[61:62], collapse = ", "))
-  cat(red("# Lymphoma Hodgkin"))
+  cat(crayon::red("# Lymphoma Hodgkin"))
   print(paste(sites_recodes$sites[63:64], collapse = ", "))
-  cat(red("# Lymphoma Non-Hodgkin"))
+  cat(crayon::red("# Lymphoma Non-Hodgkin"))
   print(paste(sites_recodes$sites[65:66], collapse = ", "))
-  cat(red("# Myeloma"))
+  cat(crayon::red("# Myeloma"))
   print(paste(sites_recodes$sites[67], collapse = ", "))
-  cat(red("# Lymphocyte Leukemia"))
+  cat(crayon::red("# Lymphocyte Leukemia"))
   print(paste(sites_recodes$sites[68:71], collapse = ", "))
-  cat(red("# Myeloid and Monocytic Leukemia"))
+  cat(crayon::red("# Myeloid and Monocytic Leukemia"))
   print(paste(sites_recodes$sites[72:75], collapse = ", "))
-  cat(red("# Other Leukemia"))
+  cat(crayon::red("# Other Leukemia"))
   print(paste(sites_recodes$sites[76:77], collapse = ", "))
-  cat(red("# Mesothelioma"))
+  cat(crayon::red("# Mesothelioma"))
   print(paste(sites_recodes$sites[78], collapse = ", "))
-  cat(red("# Kaposi Sarcoma"))
+  cat(crayon::red("# Kaposi Sarcoma"))
   print(paste(sites_recodes$sites[79], collapse = ", "))
-  cat(red("# Miscellaneous and Invalid"))
+  cat(crayon::red("# Miscellaneous and Invalid"))
   print(paste(sites_recodes$sites[80:81], collapse = ", "))
 }
 
@@ -153,4 +183,4 @@ listPrimarySites <- function() {
 #              30000, 31010, 31040, 32010, 32020, 33011, 33012, 33041, 33042, 34000, 35011, 35012, 35013, 35021, 35031, 35022, 35023, 35041, 35043,
 #              36010, 36020, 37000, 99999)
 # sites_recodes <- as.data.frame(cbind(sites, recodes), stringsAsFactors = FALSE)
-# saveRDS(sites_recodes, "sites_recodes.RData")
+# save(sites_recodes, file = "sites_recodes.RData")
