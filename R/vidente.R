6121f98ee53d8ca603b6143553a35e6f5bb79974
Marcel Ribeiro Dantas <mribeirodantas@protonmail.com>
2019-03-05 15:47:46 +0100

    Update package manuals
    Created manual for package and updated manual for preprocessSEER that
    was not included in commit 279d279f98492f5af53b483955ece9835371f9ce

---
+++ R/vidente.R
@@ -0,0 +1,14 @@
+#' vidente: A package for parsing and analyzing SEER data.
+#'
+#' The vidente package provides two categories of important functions.
+#' Functions to parse SEER data and functions to analyze it.
+#' 
+#' @section Parsing data:
+#' The foo functions ...
+#'
+#' @section Analyzing data:
+#' The bar functions ...
+#'
+#' @docType package
+#' @name vidente
+NULL
